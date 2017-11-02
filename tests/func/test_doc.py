303a59a84b45f7835822463bde42fb4c7fd47b8a
Bastien Sevajol <sevajol.bastien@gmail.com>
2017-11-02 15:22:08 +0100

    add test for doc generation when input_files + input_body both used on same controller

--- tests/func/test_doc.py
+++ tests/func/test_doc.py
@@ -91,3 +91,36 @@ class TestDocGeneration(Base):
         assert 'produce' in doc['paths']['/avatar']['get']
         assert 'image/jpeg' in doc['paths']['/avatar']['get']['produce']
         assert 200 in doc['paths']['/avatar']['get']['responses']
+
+    def test_func__input_files_doc__ok__one_file_and_text(self):
+        hapic = Hapic()
+        hapic.set_context(MyContext())
+        app = bottle.Bottle()
+
+        class MySchema(marshmallow.Schema):
+            name = marshmallow.fields.String(required=True)
+
+        class MyFilesSchema(marshmallow.Schema):
+            file_abc = marshmallow.fields.Raw(required=True)
+
+        @hapic.with_api_doc()
+        @hapic.input_files(MyFilesSchema())
+        @hapic.input_body(MySchema())
+        def my_controller(hapic_data=None):
+            assert hapic_data
+            assert hapic_data.files
+
+        app.route('/upload', method='POST', callback=my_controller)
+        doc = hapic.generate_doc(app)
+
+        assert doc
+        assert '/upload' in doc['paths']
+        assert 'consume' in doc['paths']['/upload']['post']
+        assert 'multipart/form-data' in doc['paths']['/upload']['post']['consume']
+        assert 'parameters' in doc['paths']['/upload']['post']
+        assert {
+                   'name': 'file_abc',
+                   'required': True,
+                   'in': 'formData',
+                   'type': 'file',
+               } in doc['paths']['/upload']['post']['parameters']
