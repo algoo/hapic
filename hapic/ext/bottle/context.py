0ee15c5421fbdfe97fb62dee2469a184f4399e12
Bastien Sevajol <bastien.sevajol@algoo.fr>
2017-11-10 16:43:36 +0100

    Bottle context: use query instead params

--- hapic/ext/bottle/context.py
+++ hapic/ext/bottle/context.py
@@ -14,7 +14,7 @@ class BottleContext(ContextInterface):
     def get_request_parameters(self, *args, **kwargs) -> RequestParameters:
         return RequestParameters(
             path_parameters=bottle.request.url_args,
-            query_parameters=bottle.request.params.dict,
+            query_parameters=bottle.request.query.dict,
             body_parameters=bottle.request.json,
             form_parameters=bottle.request.forms,
             header_parameters=bottle.request.headers,
