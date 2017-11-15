9e808fb0789e2f0b57343b5a908c940e6008141a
Bastien Sevajol <sevajol.bastien@gmail.com>
2017-11-15 21:29:25 +0100

    add TODOs and resolve import problems

--- hapic/ext/flask/context.py
+++ hapic/ext/flask/context.py
@@ -3,29 +3,32 @@ import json
 import typing
 from http import HTTPStatus
 
-from flask import request, Response
-
-
 from hapic.context import ContextInterface
 from hapic.exception import OutputValidationException
 from hapic.processor import RequestParameters, ProcessValidationError
 
+if typing.TYPE_CHECKING:
+    from flask import Response
+
 
 class FlaskContext(ContextInterface):
     def get_request_parameters(self, *args, **kwargs) -> RequestParameters:
+        from flask import request
         return RequestParameters(
             path_parameters=request.view_args,
             query_parameters=request.args,  # TODO: Check
             body_parameters=request.get_json(),  # TODO: Check
             form_parameters=request.form,
             header_parameters=request.headers,
+            files_parameters={},  # TODO: BS 20171115: Code it
         )
 
     def get_response(
         self,
         response: dict,
         http_code: int,
-    ) -> Response:
+    ) -> 'Response':
+        from flask import Response
         return Response(
             response=json.dumps(response),
             mimetype='application/json',
@@ -51,3 +54,6 @@ class FlaskContext(ContextInterface):
             mimetype='application/json',
             status=int(http_code),
         )
+
+# TODO BS 20171115: Implement other context methods
+# (take source in example_a_flask.py)
