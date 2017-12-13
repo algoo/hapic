d763db9b26cba50116191501a0e6888e45cd9d6e
Guénaël Muller <guenael.muller@algoo.fr>
2017-12-13 14:36:05 +0100

    Prettify result dict for fake_api test

--- tests/func/fake_api/common.py
+++ tests/func/fake_api/common.py
@@ -1,2 +1,92 @@
 from collections import OrderedDict
-SWAGGER_DOC_API = {'paths': OrderedDict([('/about', {'get': {'description': 'General information about this API.', 'responses': {200: {'schema': {'$ref': '#/definitions/AboutResponseSchema'}, 'description': 'HTTPStatus.OK'}}}}), ('/users', {'get': {'description': 'Obtain users list.', 'responses': {200: {'schema': {'$ref': '#/definitions/ListsUserSchema'}, 'description': 'HTTPStatus.OK'}}}}), ('/users/{id}', {'get': {'description': 'Obtain one user', 'responses': {200: {'schema': {'$ref': '#/definitions/UserSchema'}, 'description': 'HTTPStatus.OK'}}, 'parameters': [{'in': 'path', 'name': 'id', 'required': True, 'type': 'integer'}]}, 'delete': {'description': 'delete user', 'responses': {204: {'schema': {'$ref': '#/definitions/NoContentSchema'}, 'description': '204'}}, 'parameters': [{'in': 'path', 'name': 'id', 'required': True, 'type': 'integer'}]}}), ('/users/', {'post': {'description': 'Add new user', 'responses': {200: {'schema': {'$ref': '#/definitions/UserSchema'}, 'description': 'HTTPStatus.OK'}}, 'parameters': [{'in': 'body', 'name': 'body', 'schema': {'$ref': '#/definitions/UserSchema'}}]}})]), 'definitions': {'ListsUserSchema': {'properties': {'pagination': {'$ref': '#/definitions/PaginationSchema'}, 'items': {'type': 'array', 'items': {'$ref': '#/definitions/UserSchema'}}, 'item_nb': {'minimum': 0, 'type': 'integer', 'format': 'int32'}}, 'type': 'object', 'required': ['item_nb']}, 'AboutResponseSchema': {'properties': {'version': {'type': 'string'}, 'datetime': {'type': 'string', 'format': 'date-time'}}, 'type': 'object', 'required': ['datetime', 'version']}, 'NoContentSchema': {'properties': {}, 'type': 'object'}, 'UserSchema': {'properties': {'display_name': {'type': 'string'}, 'first_name': {'type': 'string'}, 'id': {'type': 'integer', 'format': 'int32'}, 'last_name': {'type': 'string'}, 'company': {'type': 'string'}, 'username': {'type': 'string'}, 'email_address': {'type': 'string', 'format': 'email'}}, 'type': 'object', 'required': ['company', 'display_name', 'email_address', 'first_name', 'id', 'last_name', 'username']}, 'PaginationSchema': {'properties': {'last_id': {'type': 'integer', 'format': 'int32'}, 'current_id': {'type': 'integer', 'format': 'int32'}, 'first_id': {'type': 'integer', 'format': 'int32'}}, 'type': 'object', 'required': ['current_id', 'first_id', 'last_id']}}, 'info': {'title': 'Fake API','description':'just an example of hapic API','version': '1.0.0'}, 'tags': [], 'swagger': '2.0', 'parameters': {}}
+SWAGGER_DOC_API = {
+ 'definitions': {
+     'AboutResponseSchema': {'properties': {
+                                'datetime': {'format': 'date-time',
+                                             'type': 'string'},
+                                'version': {'type': 'string'}
+                                },
+                             'required': ['datetime', 'version'],
+                             'type': 'object'},
+     'ListsUserSchema': {'properties':  {
+                            'item_nb': {'format': 'int32',
+                                        'minimum': 0,
+                                        'type': 'integer'},
+                            'items': {
+                                'items': {'$ref': '#/definitions/UserSchema'},
+                                'type': 'array'},
+                            'pagination': {'$ref': '#/definitions/PaginationSchema'}},
+                         'required': ['item_nb'],
+                         'type': 'object'},
+     'NoContentSchema': {'properties': {},
+                         'type': 'object'},
+     'PaginationSchema': {'properties': {
+                            'current_id': {'format': 'int32', 'type': 'integer'},
+                            'first_id': {'format': 'int32', 'type': 'integer'},
+                            'last_id': {'format': 'int32', 'type': 'integer'}},
+                          'required': ['current_id', 'first_id', 'last_id'],
+                          'type': 'object'},
+     'UserSchema': {'properties': {
+                        'company': {'type': 'string'},
+                        'display_name': {'type': 'string'},
+                        'email_address': {'format': 'email', 'type': 'string'},
+                        'first_name': {'type': 'string'},
+                        'id': {'format': 'int32', 'type': 'integer'},
+                        'last_name': {'type': 'string'},
+                        'username': {'type': 'string'}},
+                    'required': ['company',
+                                 'display_name',
+                                 'email_address',
+                                 'first_name',
+                                 'id',
+                                 'last_name',
+                                 'username'],
+                    'type': 'object'}},
+ 'info': {'description': 'just an example of hapic API',
+          'title': 'Fake API',
+          'version': '1.0.0'},
+ 'parameters': {},
+ 'paths': OrderedDict(
+     [('/about', {
+         'get': {
+           'description': 'General information about this API.',
+           'responses': {200: {
+               'description': 'HTTPStatus.OK',
+               'schema': {'$ref': '#/definitions/AboutResponseSchema'}}}}}),
+      ('/users', {
+         'get': {
+           'description': 'Obtain users list.',
+           'responses': {200: {
+               'description': 'HTTPStatus.OK',
+               'schema': {'$ref': '#/definitions/ListsUserSchema'}}}}}),
+      ('/users/{id}', {
+         'delete': {
+           'description': 'delete user',
+           'parameters': [{'in': 'path',
+                           'name': 'id',
+                           'required': True,
+                           'type': 'integer'}],
+           'responses': {204: {
+               'description': '204',
+               'schema': {'$ref': '#/definitions/NoContentSchema'}}}},
+         'get': {
+             'description': 'Obtain one user',
+             'parameters': [{'in': 'path',
+                             'name': 'id',
+                             'required': True,
+                             'type': 'integer'}],
+             'responses': {200: {
+                 'description': 'HTTPStatus.OK',
+                 'schema': {'$ref': '#/definitions/UserSchema'}}}}}),
+      ('/users/', {
+         'post': {
+             'description': 'Add new user',
+             'parameters': [{'in': 'body',
+                             'name': 'body',
+                             'schema': {'$ref': '#/definitions/UserSchema'}}],
+             'responses': {200: {
+                 'description': 'HTTPStatus.OK',
+                 'schema': {'$ref': '#/definitions/UserSchema'}}}}})]),
+ 'swagger': '2.0',
+ 'tags': []
+}
