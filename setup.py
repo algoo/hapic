555512f9a6e82853ae82fd270733bddb8a331a08
Bastien Sevajol <bastien.sevajol@algoo.fr>
2017-12-27 16:59:37 +0100

    v0.24

--- setup.py
+++ setup.py
@@ -35,7 +35,7 @@ setup(
     # Versions should comply with PEP440.  For a discussion on single-sourcing
     # the version across setup.py and the project code, see
     # https://packaging.python.org/en/latest/single_source_version.html
-    version='0.23',
+    version='0.24',
 
     description='HTTP api input/output manager',
     # long_description=long_description,
