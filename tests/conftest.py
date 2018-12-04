a0b36d8ab9da55f48f4c7ee345c0fc1a8d9a1bf5
Bastien Sevajol <bastien.sevajol@algoo.fr>
2018-12-04 11:46:15 +0100

    tests: add tests about serpyco and error handling

--- tests/conftest.py
+++ tests/conftest.py
@@ -7,5 +7,6 @@ collect_ignore = []
 if sys.version_info < (3, 6):
     collect_ignore.extend([
         "func/fake_api/test_fake_api_aiohttp_serpyco.py",
-        "func/test_doc_serpyco.py"
+        "func/test_doc_serpyco.py",
+        "func/test_serpyco_errors.py",
     ])
